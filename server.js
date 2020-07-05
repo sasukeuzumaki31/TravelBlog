5e759e8dfcc8b67aad6f306d61f02a6859b8bd3c
sasukeuzumaki31 <nimeshnairns@gmail.com>
2020-07-05 12:30:09 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb+srv://nimesh:9833037437@cluster0-mmitp.mongodb.net/Blog?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+app.get('/aboutme', async (req, res) => {
+  res.render('articles/about_me')
+})
+app.use('/articles', articleRouter)
+
+app.listen(5000)
