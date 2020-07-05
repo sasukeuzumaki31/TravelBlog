4060f788ac6fd68ce9b0ec68344913cd076db25e
sasukeuzumaki31 <nimeshnairns@gmail.com>
2020-07-05 12:39:04 +0530

    Port added

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.get('/aboutme', async (req, res) => {
   res.render('articles/about_me')
 })
 app.use('/articles', articleRouter)
-
-app.listen(5000)
+let port = (process.env.PORT || '5000')
+app.listen(port ,process.env.IP)
